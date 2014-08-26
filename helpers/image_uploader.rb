a275cdaad19412114aaa4b6e87194a23a1e0317c
Dain Hall <dainovu3@gmail.com>
2014-08-26 17:52:22 -0500

    Added fonts and logo

--- helpers/image_uploader.rb
+++ helpers/image_uploader.rb
@@ -1,5 +1,7 @@
 require 'pry'
 
 def load_images
-  Dir["source/images/*"].map { |image| File.basename(image) }
+  Dir["source/images/media_photos/*"].map do |image| 
+    "media_photos/#{File.basename(image)}"
+  end
 end
