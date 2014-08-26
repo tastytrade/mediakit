b6207687662fb78bc30d797461b1ff17993ac0e6
Dain Hall <dainovu3@gmail.com>
2014-08-26 18:48:11 -0500

    Added images to main path

--- helpers/image_uploader.rb
+++ helpers/image_uploader.rb
@@ -1,7 +1,16 @@
 require 'pry'
 
 def load_images
-  Dir["source/images/media_photos/*"].map do |image| 
-    "media_photos/#{File.basename(image)}"
+  Dir["source/images/media_photos/full/*"].map do |full_image_name| 
+    file_name = File.basename(full_image_name)
+    ext = File.extname(full_image_name)
+    file_without_ext = file_name.gsub(ext, "")
+    file_name = "#{file_without_ext}@2x#{ext}"
+
+    {
+      :thumb => "/images/media_photos/thumbs/#{file_name}",
+      :full => full_image_name.gsub("source/", ""),
+      :file_name => file_without_ext
+    }
   end
 end
